e696797c676f808ddf8f91b7b8616a0df613f9c6
Alin Mungra <alinmungra02@gmail.com>
2026-09-21 08:52:44 +0530

    Initial commit

---
+++ DataStore.py
@@ -0,0 +1,96 @@
+#1.Variable
+
+'''Data store'''
+print("--------Program-1---------------")
+
+name = "Alin"
+age = 20
+city = "Rajkot"
+
+print("Name:",name)
+print("Age:",age)
+print("City:",city)
+print("\n")
+
+
+'''2. Swap the values of two variables.'''
+print("--------Program-2---------------")
+a=5
+b=20
+
+a,b = b,a
+
+print("a:",a)
+print("b:",b)
+print("\n")
+
+
+'''3. Calculate the area of a rectangle using variables.'''
+print("--------Program-3---------------")
+
+length = 10
+width = 2
+
+area = length *width
+
+print("The area of the rectangle is :",area)
+print("\n")
+
+
+
+
+
+'''4. Calculate simple interest using variables.'''
+print("--------Program-4---------------")
+principal =2000
+rate = 5
+time = 2
+
+simple_interest =(principal * rate * time) /100
+print("the simple interest is:",simple_interest)
+print("\n")
+
+
+
+'''5. Convert Celsius temperature to Fahrenheit.'''
+print("--------Program-5---------------")
+
+celsius = 25
+
+fahrenheit =(celsius * 9/5) + 32
+
+print("Temperature in Fahrenheit:",fahrenheit)
+print("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
