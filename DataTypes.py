7d932159684212769d54681e1073ab24ba9902d2
Alin Mungra <alinmungra02@gmail.com>
2026-09-21 08:52:45 +0530

    Added DataTypes

---
+++ DataTypes.py
@@ -0,0 +1,52 @@
+#2. Data Types
+
+'''1. Demonstrate int, float, str, bool, and complex.'''
+print("--------Program-1---------------")
+my_int = 10
+print(my_int,type(my_int))
+
+my_float = 10.5
+print(my_float,type(my_float))
+
+my_str = "Hello"
+print(my_str,type(my_str))
+
+my_bool = True
+print(my_bool,type(my_bool))
+
+my_complex = 3 +4j
+print(my_complex,type(my_complex))
+
+
+
+'''2. Accept two numbers and display their data types.'''
+print("--------Program-2---------------")
+num1=input("Enter First Number=")
+num2=input("Enter Second Number=")
+
+print("First input type:",type(num1))
+print("Second input type:",type(num2))
+
+print("\n")
+
+
+
+
+
+
+
+
+
+'''3. Convert a string number into an integer and float.'''
+print("--------Program-3---------------")
+print("\n")
+
+
+'''4. Find the length of a string.'''
+print("--------Program-4---------------")
+print("\n")
+
+
+'''5. Create a list, tuple, set, and dictionary and display their types.'''
+print("--------Program-5---------------")
+print("\n")
